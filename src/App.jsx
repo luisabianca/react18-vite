e85f88572be1e24c21a3b96c6e15f7a17e84aa76
Bianca Santos <luisabianca93@gmail.com>
2023-11-11 01:53:39 -0300

    first project using usestate for birthday list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import ListBirthday from './components/List Birthday/ListBirthday';
 
 function App() {
   return (
     <>
-      Hello World
+      <ListBirthday />
     </>
   )
 }
